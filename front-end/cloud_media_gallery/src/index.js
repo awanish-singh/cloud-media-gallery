ec3a9c07053dd62f313b99d804d4ffe8f6f6cfd4
Shadab <shadab.alam2@cognizant.com>
2022-05-10 00:08:13 +0530

    adding routes

--- front-end/cloud_media_gallery/src/index.js
+++ front-end/cloud_media_gallery/src/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import HomePage from "./components/HomePage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}/>
+        <Route path="/home" element={<HomePage/>}/>
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
