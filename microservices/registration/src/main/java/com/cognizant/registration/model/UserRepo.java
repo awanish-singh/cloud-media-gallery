4436d08839b816a7b4a7eea3616784f2c7b7464d
Shadab <shadab.alam2@cognizant.com>
2022-05-18 18:51:21 +0530

    added api for login&register

--- microservices/registration/src/main/java/com/cognizant/registration/model/UserRepo.java
+++ microservices/registration/src/main/java/com/cognizant/registration/model/UserRepo.java
@@ -1,8 +1,9 @@
-package com.cognizant.loginmvc.model;
+package com.cognizant.registration.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Integer>{
-
+	
 	public User findByUserName(String userName);
+
 }
