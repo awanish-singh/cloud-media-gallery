2af1cd5ab5d3347dbfc57870b79d510d612b4ced
Shadab <shadab.alam2@cognizant.com>
2022-05-21 14:58:05 +0530

    added necessary services 1.api service 2.eureka 3.api-gateway for backend

--- microservices/registration/src/main/java/com/cognizant/registration/model/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.registration.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Integer>{
-	
-	public User findByUserName(String userName);
-
-}
